7528356953d1faaab7723e442f86f8dcab3fbae2
SunilAinlod <sunil.ainlod@gmail.com>
2018-06-11 09:32:15 +0530

    Create App.java

---
+++ App.java
@@ -0,0 +1,8 @@
+class CharRead
+{
+ public static void main( String args[])
+ {
+  BufferedReader br = new Bufferedreader(new InputstreamReader(System.in));
+  char c = (char)br.read();       //Reading character 
+ }
+}
